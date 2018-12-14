ca0f6ba73522eee2ab42820186971492de3da8cd
hsonne <hauke.sonnenberg@gmx.de>
2018-12-14 07:18:25 +0100

    Clean test-codemeta_description.R
    - Remove all "testthat::" as the package is loaded
      during testing. This shortens the tests and
      increases readability
    - Use helper functions to avoid repetition
    - Use string constants to keep the tests short and
      clean
    - Combine tests where appropriate. Instead of
        expect_true(a %in% c)
        expect_true(b %in% c)
      use
        expect_true(all(c(a, b) %in% c))

--- tests/testthat/test-codemeta_description.R
+++ tests/testthat/test-codemeta_description.R
@@ -1,84 +1,90 @@
-testthat::context("codemeta_description.R")
+context("codemeta_description.R")
 
-testthat::test_that("We can use a preset id", {
-  f <- package_file("codemetar", "DESCRIPTION")
-  codemeta_description(f, id = "https://doi.org/10.looks.like/doi")
+# Helper functions
+example_description <- function(x) codemeta_description(example_file(x))
+
+check_maintainer_and_n_authors <- function(x, name, n) {
+  expect_true(x$maintainer[[1]]$familyName == name)
+  expect_equal(length(x$author), n)
+}
+
+# String constants
+url_1 <- "https://doi.org/10.looks.like/doi"
+url_2 <- "https://github.com/ropensci/essurvey"
+url_3 <- "https://ropensci.github.io/essurvey/"
+url_4 <- "https://github.com/ropensci/codemetar"
+url_5 <- "https://ropensci.github.io/codemetar"
+url_6 <- "https://github.com/ropensci/codemetar#codemetar"
+url_7 <- "https://ropensci.org"
+
+test_that("We can use a preset id", {
+  codemeta_description(package_file("codemetar", "DESCRIPTION"), id = url_1)
 })
 
-testthat::test_that("several URLs", {
-  cm <- codemeta_description(example_file("DESCRIPTION_two_URLs"))
-  expect_equal(cm$codeRepository, "https://github.com/ropensci/essurvey")
-  expect_true("https://ropensci.github.io/essurvey/" %in%
-                cm$relatedLink)
+test_that("several URLs", {
+  codemeta <- example_description("DESCRIPTION_two_URLs")
+  expect_equal(codemeta$codeRepository, url_2)
+  expect_true(url_3 %in% codemeta$relatedLink)
 })
 
-testthat::test_that("no direct link to README", {
-  cm <- codemeta_description(example_file("DESCRIPTION_good_readmeinurl"))
-  expect_equal(cm$codeRepository, "https://github.com/ropensci/codemetar")
-  expect_true("https://ropensci.github.io/codemetar" %in%
-                cm$relatedLink)
-  expect_true("https://github.com/ropensci/codemetar#codemetar" %in%
-                cm$relatedLink)
+test_that("no direct link to README", {
+  codemeta <- example_description("DESCRIPTION_good_readmeinurl")
+  expect_equal(codemeta$codeRepository, url_4)
+  expect_true(all(c(url_5, url_6) %in% codemeta$relatedLink))
 })
 
-testthat::test_that("We can parse additional terms", {
-  cm <- codemeta_description(example_file("DESCRIPTION_ex1.dcf"))
-  testthat::expect_equal(length(cm$keywords), 6)
-  testthat::expect_equal(cm$isPartOf, "https://ropensci.org")
+test_that("We can parse additional terms", {
+  codemeta <- example_description("DESCRIPTION_ex1.dcf")
+  expect_equal(length(codemeta$keywords), 6)
+  expect_equal(codemeta$isPartOf, url_7)
 })
 
-testthat::test_that("We can parse plain Authors: & Maintainers: entries", {
+test_that("We can parse plain Authors: & Maintainers: entries", {
 
-  authors <- codemeta_description(example_file("DESCRIPTION_ex1.dcf"))
-  expect_true(authors$maintainer[[1]]$familyName == "Boettiger")
-  expect_equal(length(authors$author), 0)
+  example_description("DESCRIPTION_ex1.dcf") %>%
+    check_maintainer_and_n_authors("Boettiger", 0)
 
-  authors <- codemeta_description(example_file("example.dcf"))
-  expect_true(authors$maintainer[[1]]$familyName == "Developer")
-  expect_equal(length(authors$author), 2)
+  example_description("example.dcf") %>%
+    check_maintainer_and_n_authors("Developer", 2)
 
-  authors <- codemeta_description(example_file("DESCRIPTION_plainauthors"))
-  expect_true(authors$maintainer[[1]]$familyName == "Ok")
-  expect_equal(length(authors$author), 2)
+  example_description("DESCRIPTION_plainauthors") %>%
+    check_maintainer_and_n_authors("Ok", 2)
 
-  authors <- codemeta_description(example_file("DESCRIPTION_twomaintainers"))
+  authors <- example_description("DESCRIPTION_twomaintainers")
   expect_equal(length(authors$author), 1)
   expect_equal(length(authors$maintainer), 2)
 })
 
-testthat::test_that("Helper functions work correctly", {
+test_that("Helper functions work correctly", {
+
+  expect_fields <- function(x, fields) expect_true(all(fields %in% names(x)))
 
   # Provide testdata
   codemeta <- new_codemeta()
   codemeta$package <- "abc"
-  descr <- desc::desc(example_file("DESCRIPTION_good"))
+  description <- desc::desc(example_file("DESCRIPTION_good"))
 
   # test add_repository_terms()
   expect_error(add_repository_terms())
-  result <- add_repository_terms(codemeta, descr)
-  expect_true(all(c("codeRepository", "relatedLink") %in% names(result)))
+  add_repository_terms(codemeta, description) %>%
+    expect_fields(c("codeRepository", "relatedLink"))
 
   # test add_language_terms()
   expect_error(add_language_terms())
-  result <- add_language_terms(codemeta)
-  expect_true(all(
-    c("programmingLanguage", "runtimePlatform") %in%
-      names(result)
-  ))
+  add_language_terms(codemeta) %>%
+    expect_fields(c("programmingLanguage", "runtimePlatform"))
 
   # test add_person_terms()
   expect_error(add_person_terms())
-  result <- add_person_terms(codemeta, descr)
-  expect_true(all(
-    c("author", "contributor", "copyrightHolder", "funder", "maintainer") %in%
-      names(result)))
+  add_person_terms(codemeta, description) %>%
+    expect_fields(c(
+      "author", "contributor", "copyrightHolder", "funder", "maintainer"
+    ))
 
   # test add_software_terms
   expect_error(add_software_terms())
-  result <- add_software_terms(codemeta, descr)
-  expect_true(all(
-    c("softwareSuggestions", "softwareRequirements") %in% names(result)
-  ))
+  add_software_terms(codemeta, description) %>%
+    expect_fields(c("softwareSuggestions", "softwareRequirements"))
 
   # test add_remote_provider()
   expect_error(add_remote_provider())
@@ -89,6 +95,6 @@ testthat::test_that("Helper functions work correctly", {
 
   # test add_additional_terms()
   expect_error(add_additional_terms())
-  result <- add_additional_terms(codemeta, descr)
-  expect_true(all(c("isPartOf", "keywords") %in% names(result)))
+  add_additional_terms(codemeta, description) %>%
+    expect_fields(c("isPartOf", "keywords"))
 })
